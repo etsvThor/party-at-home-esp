1a316bf3db48203f8ba75da6f11d98c896e0dda5
rutgervana <rutgervana@live.nl>
2021-05-06 13:48:28 +0200

    Changed begin method to use SSL

--- src/main.cpp
+++ src/main.cpp
@@ -97,7 +97,7 @@ void setup() {
     wifiManager.autoConnect("ThorpartyAP");
 
 	// server address, port and URL
-	webSocket.begin("wss://party-at-home.thor.edu", 443 , "/ws/chat/henk/");
+	webSocket.beginSSL("party-at-home.thor.edu", 443 , "/ws/chat/henk/");
 
 	// event handler
 	webSocket.onEvent(webSocketEvent);
